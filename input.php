57b2295ddce5fe1d19583473b869a8683786be7b
ZilmikafahD <dias.mika@yahoo.com>
2018-04-24 18:12:13 +0700

    uts

---
+++ input.php
@@ -0,0 +1,7 @@
+<form action = "simpanbaru.php" method="post">
+    KODE : <input type="text" name="kode"><br>
+    NAMA BARANG : <input type="text" name="namaBarang"><br>
+    STOK : <input type"text" name="stok"><br>
+    </table>
+    <input type="submit" value="simpan">
+    </form> 
